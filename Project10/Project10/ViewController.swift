374dbad47a5adb2775df61c85eaf2e61be9911ca
Dillon McElhinney <dillon.mce@gmail.com>
2019-06-28 14:34:37 -0600

    Finish Day 43 work

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController:
+    UICollectionViewController,
+    UINavigationControllerDelegate,
+    UIImagePickerControllerDelegate {
+    
+    var people: [Person] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add,
+                            target: self,
+                            action: #selector(addNewPerson))
     }
 
 
     override func collectionView(_ collectionView: UICollectionView,
                                  numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
 
     override func collectionView(_ collectionView: UICollectionView,
@@ -27,8 +36,75 @@ class ViewController: UICollectionViewController {
                                                for: indexPath) as? PersonCollectionViewCell else {
             fatalError("Unable to dequeue a PersonCollectionViewCell. Check for typos")
         }
+        let person = people[indexPath.item]
+
+        cell.nameLabel.text = person.name
+
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let alertController = UIAlertController(title: "Rename Person",
+                                                message: nil, preferredStyle: .alert)
+        alertController.addTextField() { textField in
+            textField.text = person.name == "Unknown" ? "" : person.name
+            textField.placeholder = "Person's Name"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        
+        let saveAction = UIAlertAction(title: "Ok", style: .default)
+        { [weak self, weak alertController] _ in
+            guard let newName = alertController?.textFields?.first?.text else { return }
+            person.name = newName
+            
+            self?.collectionView.reloadItems(at: [indexPath])
+        }
+        
+        alertController.addAction(saveAction)
+        present(alertController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown",
+                            image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        dismiss(animated: true)
+}
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory,
+                                        in: .userDomainMask).first!
     }
     
 }
