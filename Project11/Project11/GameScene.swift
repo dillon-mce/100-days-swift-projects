e62eb2809cd112fcac46fcf9a273169c820fb18b
Dillon McElhinney <dillon.mce@gmail.com>
2019-07-03 23:23:51 -0600

    Finish Day 45 work

---
+++ Project11/Project11/GameScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Dillon McElhinney on 7/3/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        for i in 0..<5 {
+            let x = i*256
+            makeBouncer(at: CGPoint(x: x, y: 0))
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+            ball.physicsBody?.restitution = 0.4
+            ball.position = location
+            addChild(ball)
+        }
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+}
