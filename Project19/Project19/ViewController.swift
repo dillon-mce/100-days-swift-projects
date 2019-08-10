c1171b3525f302eb729a31cbadc4ce9fe99bf7f8
Dillon McElhinney <dillon.mce@gmail.com>
2019-08-10 13:24:53 -0600

    Finish Day 67 work

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Dillon McElhinney on 8/10/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
