b34cf1743aed8c34ce3b7778927dc2cb1f1696d8
Dillon McElhinney <dillon.mce@gmail.com>
2019-08-10 21:51:15 -0600

    Finish Day 68 work

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -11,24 +11,81 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    var pageTitle = ""
+    var pageURL = ""
 
+    @IBOutlet weak var scriptView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .done,
+                            target: self,
+                            action: #selector(done))
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
+
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
 
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
-                    
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
                 }
             }
         }
     }
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": scriptView.text ?? ""]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary,
+                                              typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        let key = UIResponder.keyboardFrameEndUserInfoKey
+        guard let keyboardValue = notification.userInfo?[key] as? NSValue else {
+                    return
+        }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame,
+                                                from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scriptView.contentInset = .zero
+        } else {
+            let bottom = keyboardViewEndFrame.height - view.safeAreaInsets.bottom
+            scriptView.contentInset = UIEdgeInsets(top: 0,
+                                                   left: 0,
+                                                   bottom: bottom,
+                                                   right: 0)
+        }
+
+        scriptView.scrollIndicatorInsets = scriptView.contentInset
+
+        let selectedRange = scriptView.selectedRange
+        scriptView.scrollRangeToVisible(selectedRange)
     }
 
 }
