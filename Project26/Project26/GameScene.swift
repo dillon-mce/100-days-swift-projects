8488cdde6d347bedbbe44f019156b21a7cf8783f
Dillon McElhinney <dillon.mce@gmail.com>
2019-09-28 11:00:18 -0600

    Finish Day 85 work

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,117 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Dillon McElhinney on 9/28/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        loadLevel()
+    }
+
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1",
+                                             withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not load level1.txt from the app bundle.")
+        }
+
+        let lines = levelString.components(separatedBy: "\n")
+
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let x = (64 * column) + 32
+                let y = (64 * row) + 32
+                let position = CGPoint(x: x, y: y)
+
+                if letter == "x" {
+                    loadWall(at: position)
+                } else if letter == "v"  {
+                    loadVortex(at: position)
+                } else if letter == "s"  {
+                    loadStar(at: position)
+                } else if letter == "f"  {
+                    loadFinish(at: position)
+                } else if letter == " " {
+                    // this is an empty space – do nothing!
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
+    }
+
+    func loadWall(at position: CGPoint) {
+        let node = SKSpriteNode(imageNamed: "block")
+        node.position = position
+
+        node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+        node.physicsBody?.isDynamic = false
+        addChild(node)
+    }
+
+    func loadVortex(at position: CGPoint) {
+        let node = SKSpriteNode(imageNamed: "vortex")
+        node.name = "vortex"
+        node.position = position
+        node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi,
+                                                        duration: 1)))
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        addChild(node)
+    }
+
+    func loadStar(at position: CGPoint) {
+        let node = SKSpriteNode(imageNamed: "star")
+        node.name = "star"
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        node.position = position
+        addChild(node)
+    }
+
+    func loadFinish(at position: CGPoint) {
+        let node = SKSpriteNode(imageNamed: "finish")
+        node.name = "finish"
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        node.position = position
+        addChild(node)
+    }
+}
